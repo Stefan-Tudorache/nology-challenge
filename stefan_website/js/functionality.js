f675619e52a57554a8ba4fe723103e54074a3f5e
Stefan <70359995+Stefan-Tudorache@users.noreply.github.com>
2020-09-18 13:48:01 +0200

    Add files via upload

---
+++ stefan_website/js/functionality.js
@@ -0,0 +1,139 @@
+class TypeWriter {
+  constructor(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = "";
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+  }
+
+  type() {
+    // Current index of word
+    const current = this.wordIndex % this.words.length;
+    // Get full text of current word
+    const fullTxt = this.words[current];
+
+    // Check if deleting
+    if (this.isDeleting) {
+      // Remove char
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      // Add char
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    // Insert txt into element
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+    // Initial Type Speed
+    let typeSpeed = 300;
+
+    if (this.isDeleting) {
+      typeSpeed /= 2;
+    }
+
+    // If word is complete
+    if (!this.isDeleting && this.txt === fullTxt) {
+      // Make pause at end
+      typeSpeed = this.wait;
+      // Set delete to true
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+      this.isDeleting = false;
+      // Move to next word
+      this.wordIndex++;
+      // Pause before start typing
+      typeSpeed = 500;
+    }
+
+    setTimeout(() => this.type(), typeSpeed);
+  }
+}
+
+// Init On DOM Load
+document.addEventListener("DOMContentLoaded", init);
+
+// Init App
+function init() {
+  const txtElement = document.querySelector(".txt-type");
+  const words = JSON.parse(txtElement.getAttribute("data-words"));
+  const wait = txtElement.getAttribute("data-wait");
+  // Init TypeWriter
+  new TypeWriter(txtElement, words, wait);
+}
+
+// Add Class Show
+
+const items = document.querySelectorAll("#about-container div");
+
+const isInViewport = (el) => {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+const run = () =>
+  items.forEach((item) => {
+    if (isInViewport(item)) {
+      item.classList.add("show");
+    } else {
+      item.classList.remove("show");
+    }
+  });
+
+// Events
+window.addEventListener("load", run);
+window.addEventListener("resize", run);
+window.addEventListener("scroll", run);
+
+// Show Navbar Background
+const navbar = document.getElementById("main-nav");
+let scrolled = false;
+
+window.onscroll = function () {
+  if (window.pageYOffset > 100) {
+    navbar.classList.remove("top");
+    if (!scrolled) {
+      navbar.style.transform = "translateY(-70px)";
+    }
+    setTimeout(function () {
+      navbar.style.transform = "translateY(0)";
+      scrolled = true;
+    }, 200);
+  } else {
+    navbar.classList.add("top");
+    scrolled = false;
+  }
+};
+
+// Smooth Scrolling
+$("#main-nav a, a, .btn").on("click", function (e) {
+  if (this.hash !== "") {
+    e.preventDefault();
+
+    const hash = this.hash;
+
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 68,
+      },
+      800
+    );
+  }
+});
+
+// Show / Set Time
+const counter = setInterval(myCounter, 1000);
+
+function myCounter() {
+  let date = new Date();
+  let timer = date.toLocaleTimeString();
+  document.getElementById("time").innerHTML = timer;
+}
